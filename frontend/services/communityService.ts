1f39e99350eed74a1d6a52e4d8569e3ce05dea14
Adrián Díaz Vázquez <adrdiavaz@alum.us.es>
2026-03-04 14:36:08 +0100

    fix: Arreglo de api url para pruebas en movil

--- frontend/services/communityService.ts
+++ frontend/services/communityService.ts
@@ -1,26 +1,30 @@
 // services/communityService.ts
 
+import { API_URL } from "@/constants/api";
+
 export interface Community {
   id: string;
   name: string;
 }
 
-export const fetchUserCommunities = async (token: string): Promise<Community[]> => {
+export const fetchUserCommunities = async (
+  token: string,
+): Promise<Community[]> => {
   // ⚠️ IMPORTANTE: CAMBIA ESTA IP POR LA TUYA (ej: 192.168.1.34)
   // Si usas el emulador de Android Studio, pon: http://10.0.2.2:8000
-  const API_URL = 'http://localhost:8000'; // o http://127.0.0.1:8000; 
-  
+  // o http://127.0.0.1:8000;
+
   try {
     const response = await fetch(`${API_URL}/users/me/communities`, {
-      method: 'GET',
+      method: "GET",
       headers: {
-        'Authorization': `Bearer ${token}`,
-        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`,
+        "Content-Type": "application/json",
       },
     });
 
     if (!response.ok) {
-      throw new Error('Error del servidor');
+      throw new Error("Error del servidor");
     }
 
     return await response.json();
