c695619a7dcb3e1e210ae9cfe0fe7ca8c788be6c
Jesus-Pons <jesponmor@alum.us.es>
2026-03-22 15:38:23 +0100

    fix: Añadido minimal para no devolver objeto feedback

--- backend/api/feedback/feedback.py
+++ backend/api/feedback/feedback.py
@@ -13,7 +13,7 @@ async def submit_feedback(feedback: Feedback, supabase: Client = Depends(get_sup
 
     insert_data = feedback.model_dump()
     try:
-        supabase.table("feedback").insert(insert_data).execute()
+        supabase.table("feedback").insert(insert_data, returning="minimal").execute()
     except Exception as e:
         print(f"Database error at feedback submission: {str(e)}")
         raise HTTPException(status_code=500, detail=f"Database error at feedback submission: {str(e)}")
