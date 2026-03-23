427ebab34dbaa2d5304fb9a97ba65af098d139f3
Adrián Díaz Vázquez <adrdiavaz@alum.us.es>
2026-03-23 14:44:26 +0100

    fix: arreglos de textos y navegacion

--- frontend/components/SidebarMenu.tsx
+++ frontend/components/SidebarMenu.tsx
@@ -132,7 +132,7 @@ export default function SidebarMenu(props: DrawerContentComponentProps) {
           name: "Comunidades",
           icon: "business-outline" as IconName,
           route: "admin",
-          requiresAdmin: true,
+          enabled: true,
         },
         {
           name: "Administración",
@@ -168,7 +168,6 @@ export default function SidebarMenu(props: DrawerContentComponentProps) {
         />
         <View style={styles.headerTextContainer}>
           <Text style={styles.title}>Vecinus</Text>
-          <Text style={styles.subtitle}>Comunidad</Text>
         </View>
       </View>
 
