e557427f813228001c3911e3d05337bf96c63d4b
DLJ7666 <jarguezduran@gmail.com>
2026-03-24 20:41:49 +0100

    fix: manejo de errores de inicio de sesión
    Para que dé 401 cuando la sesión es inválida

--- backend/api/auth/login.py
+++ backend/api/auth/login.py
@@ -2,6 +2,7 @@ from core.deps import get_supabase_anon
 from fastapi import APIRouter, Depends, HTTPException
 from schemas.auth.auth import UserLogin
 from supabase import Client
+from supabase_auth.errors import AuthApiError
 
 router = APIRouter()
 
@@ -10,9 +11,19 @@ router = APIRouter()
 def login(user: UserLogin, supabase: Client = Depends(get_supabase_anon)):
     try:
         session = supabase.auth.sign_in_with_password({"email": user.email, "password": user.password})
+
+        if not getattr(session, "session", None):
+            raise HTTPException(status_code=401, detail="Credenciales incorrectas")
+
         return session
+    except HTTPException:
+        raise
+    except AuthApiError as aae:
+        if aae.code == "invalid_credentials":
+            raise HTTPException(status_code=401, detail="Credenciales incorrectas")
+        raise HTTPException(status_code=500, detail=f"Error de autenticacion con Supabase: {str(aae)}")
     except Exception as e:
-        raise HTTPException(status_code=500, detail=f"Database error at login: {str(e)}")
+        raise HTTPException(status_code=500, detail=f"Error interno al iniciar sesion: {str(e)}")
 
 
 @router.post("/logout")
